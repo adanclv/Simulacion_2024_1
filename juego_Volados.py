0759e88462cc92d6f4c54083a4d14f564d0e0763
adanclv <adanclv11@hotmail.com>
2024-04-18 17:01:01 -0600

    fix volados

--- juego_Volados.py
+++ juego_Volados.py
@@ -3,19 +3,20 @@ import random as rand
 
 def juegoVolado(cash):
     lost = 1
-    win = 0
-    while cash > 0 and win < 3:
-        x = rand.randint(0, 1)
-        if x == 0:
+    while cash > 0 and cash < 50:
+        x = rand.uniform(0.0, 1.0)
+        if x > 0.5:
             if cash < (10 * lost):
                 cash -= cash
             else:
                 cash -= 10 * lost
                 lost *= 2
         else:
-            cash += 10 * lost
+            if cash < (10 * lost):
+                cash += cash
+            else:
+                cash += 10 * lost
             lost = 1
-            win += 1
 
     return cash
 
